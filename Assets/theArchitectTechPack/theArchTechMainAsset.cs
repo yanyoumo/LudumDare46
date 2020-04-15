6e4acf2c7d77594778de576927fc8f49e9170a1c
yanyoumo <yanyoumo@gmail.com>
2020-04-15 23:02:13 +0800

    那这个项目作为ArchTechPack的试验田

---
+++ Assets/theArchitectTechPack/theArchTechMainAsset.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "theArchTechMainAsset", menuName = "theArchTechPack/theArchTechMainAsset", order = 51)]
+public class theArchTechMainAsset : ScriptableObject
+{
+    
+}
