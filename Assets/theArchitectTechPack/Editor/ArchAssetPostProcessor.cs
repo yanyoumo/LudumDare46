a9f7e2a8831eb150c1634d6c3a60e4d98287ea13
yanyoumo <yanyoumo@gmail.com>
2020-04-17 18:16:44 +0800

    AssetProcessor更新

--- Assets/theArchitectTechPack/Editor/ArchAssetPostProcessor.cs
+++ Assets/theArchitectTechPack/Editor/ArchAssetPostProcessor.cs
@@ -8,6 +8,32 @@ namespace theArchitectTechPack
         void OnPreprocessTexture()
         {
             //TODO 线性空间和法线控制那些东西。但是估计LD46用不上。
+            TextureImporter textureImporter = assetImporter as TextureImporter;
+            System.Diagnostics.Debug.Assert(textureImporter != null, nameof(textureImporter) + " != null");
+            if (assetPath.ToLower().Contains(".hdr"))
+            {
+                textureImporter.textureShape = TextureImporterShape.TextureCube;
+                textureImporter.sRGBTexture = true;
+            }
+
+            if (assetPath.ToLower().Contains("_basecolor.tga"))
+            {
+                textureImporter.textureShape = TextureImporterShape.Texture2D;
+                textureImporter.sRGBTexture = true;
+                textureImporter.textureType = TextureImporterType.Default;
+            }
+            if (assetPath.ToLower().Contains("_maskmap.tga"))
+            {
+                textureImporter.textureShape = TextureImporterShape.Texture2D;
+                textureImporter.sRGBTexture = false;
+                textureImporter.textureType = TextureImporterType.Default;
+            }
+            if (assetPath.ToLower().Contains("_normal.tga"))
+            {
+                textureImporter.textureShape = TextureImporterShape.Texture2D;
+                textureImporter.sRGBTexture = false;
+                textureImporter.textureType = TextureImporterType.NormalMap;
+            }
         }
 
         void OnPreprocessModel()
