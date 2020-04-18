f97ce9655e0ea2b30f8806ce8ecb6daea4131d87
yanyoumo <yanyoumo@gmail.com>
2020-04-18 21:23:32 +0800

    玩家移动完成，UI完成一部分
    玩家现在可以进行常规的移动了，并且UI可以显示视觉和听觉的提示了。

--- Assets/Script/_Common/GlobalHelper.cs
+++ Assets/Script/_Common/GlobalHelper.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using theArchitectTechPack.GlobalHelper;
 
+namespace theArch_LD46
+{
+    public enum SenseType
+    {
+        Vision,
+        Audio,
+        Feeling,
+        Compass,
+    }
+
+    public static class theArch_LD46_Time
+    {      
+        public static float Time;//In second.
+        public static float delTime;//In second.
+    }
+
+}
+
 namespace theArch_LD46.GlobalHelper
 {
     public static partial class StaticName
     {
         //
-        public static readonly string INPUT_AXIS_NAME_FORWARD = "Fire";
-        public static readonly string INPUT_AXIS_NAME_LEFR = "Fire";
+        public static readonly string INPUT_AXIS_NAME_FORWARD = "MoveForward";
+        public static readonly string INPUT_AXIS_NAME_LEFR = "MoveLeft";
 
         public static readonly int SCENE_ID_GAMEPLAY = 0;
     }
