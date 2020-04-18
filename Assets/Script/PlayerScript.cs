fd0a2275f0b0083db9f1788dcbe89d5c8d03f08a
yanyoumo <yanyoumo@gmail.com>
2020-04-18 16:07:22 +0800

    上传基础框架

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using theArch_LD46.GlobalHelper;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    //TODO：田子，读取键盘输入，让玩家基于速度移动。留出速度，位置等公开API。需要对当地中带有collider的墙有反应。
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string AxisName=StaticName.INPUT_AXIS_NAME_FORWARD;
+    }
+}
--- Assets/Script/_Common/GlobalHelper.cs
+++ Assets/Script/_Common/GlobalHelper.cs
@@ -8,7 +8,8 @@ namespace theArch_LD46.GlobalHelper
     public static partial class StaticName
     {
         //
-        public static readonly string INPUT_BUTTON_NAME_FIRE = "Fire";
+        public static readonly string INPUT_AXIS_NAME_FORWARD = "Fire";
+        public static readonly string INPUT_AXIS_NAME_LEFR = "Fire";
 
         public static readonly int SCENE_ID_GAMEPLAY = 0;
     }
